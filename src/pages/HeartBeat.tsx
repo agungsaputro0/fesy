e7812a2ced0f478508368e2d424abd911cca0d20
Agung Saputro <agung.saputro888@outlook.com>
2025-03-17 16:31:36 +0700

    update 17-03-2025

--- src/pages/HeartBeat.tsx
+++ src/pages/HeartBeat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { 
   Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, StandardMaterial, Texture 
 } from "@babylonjs/core";
